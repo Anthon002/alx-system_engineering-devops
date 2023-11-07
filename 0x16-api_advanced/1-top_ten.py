f9dc1b31a72d34e0cfbf1751b113600a026960fa
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-11-07 23:42:05 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""This script will to return top posts from a subreddit."""
+"""This script will print top posts from a subreddit."""
 import requests as reqs
 
 
 def top_ten(subreddit):
-    """This method will return the titles of the 10 top posts on a subreddit."""
+    """This method will print the titles of the 10 top posts from a subreddit."""
     uri = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    parameter = {
+    params = {
         "limit": 10
     }
-    response = reqs.get(uri, headers=headers, parameter=parameter,
+    response = reqs.get(uri, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
-    _res = response.json().get("data")
-    [print(c.get("data").get("title")) for c in _res.get("children")]
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
