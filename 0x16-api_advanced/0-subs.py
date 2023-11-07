bcf93dd320937216dbab52f5e09cc4f514aaa454
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2023-11-07 22:51:08 +0100

    query reddit subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this script will  query subscribers from a  subreddit."""
+import req as req 
+
+def number_of_subscribers(subreddit):
+    """This will return  total no. of subs from a given subreddit."""
+    uri = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    responses = req.get(uri, headers=headers, allow_redirects=False)
+    if responses.status_code == 404:
+        return 0
+    else:
+      results = responses.json().get("data")
+      return results.get("subscribers")
